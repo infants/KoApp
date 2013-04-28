440f1627d11d3b3f8214f773483569e58f9d89ec
Infants <webmaster@infnet.id.lv>
2013-04-28 16:31:50 +0400

    Started template controller. Added 960 stylesheets

---
+++ KH/app/classes/Controller/Catalog.php
@@ -0,0 +1,12 @@
+<?php
+
+class Controller_Catalog extends Controller_Template {
+
+  public function action_index()
+  {
+    echo "Hello World";
+  }
+
+}
+
+// End Controller_Catalog
---
+++ KH/app/classes/Controller/Template.php
@@ -0,0 +1,16 @@
+<?php
+
+class Controller_Template extends Kohana_Controller_Template {
+
+  public $template = 'site';
+
+  public function before()
+  {
+    parent::before();
+  }
+
+  public function after()
+  {
+    parent::after();
+  }
+}
